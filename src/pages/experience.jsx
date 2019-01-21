fd3385ad47ee55d00ba534b05d9711ff7962797e
Waleed-BS <waleed-bs@live.com>
2019-01-21 18:50:06 -0500

    feat: add to experience page

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -14,22 +14,26 @@ class Experience extends React.Component {
         <div className="experience__list-container">
           <ul>
             <li>
-              Developed a <span>game</span> to recognize people’s reactive
-              response to hearing words in a non-native language.
+              Developed a game to recognize people’s reactive response to
+              hearing words in a non-native language.
             </li>
             <li>
-              Built a <span>multiplayer server</span> on a <u>Node.js</u>{' '}
-              backend using WebSocket protocol to provide real-time
-              communication between players and server.
+              Built a multiplayer server on a <span>Node.js</span> backend using
+              WebSocket protocol to provide real-time communication between
+              players and server.
             </li>
             <li>
-              Implemented the client side of the game by utilizing <u>React</u> {' '}
-              components along with <u>Redux</u> library for managing the UI
-              state.
+              Implemented the client side of the game by taking advantage of{' '}
+              <span>React</span> components along with <span>Redux</span>{' '}
+              for managing the state of players and rooms.
             </li>
             <li>
               Players are able to play the game by creating or joining a room of
               two people after specifying their name and native country.
+            </li>
+            <li>
+              This project assisted members of Department of Computer Science by
+              utilizing it as a part of a research project.
             </li>
           </ul>
         </div>
@@ -55,21 +59,19 @@ class Experience extends React.Component {
           <ul>
             <li>
               Led a team of two while working with a local organization to
-              develop a website from scratch using the <u>LAMP stack</u>.
+              develop a website from scratch using the <span>LAMP stack</span>.
             </li>
             <li>
-              Developed to <span>automate</span> the process of Social Return
-              on Investment (SROI) and Economic Investment (EI){' '}
-              <span>surveys</span>.
+              Developed to automate the process of Social Return on Investment
+              (SROI) and Economic Investment (EI) surveys.
             </li>
             <li>
-              Designed to let the administrator to have full control over {' '}
-              <span>managing surveys</span> while <span>limiting the access</span> of regular users.
+              Designed to let the administrator to have full control over{' '}
+              managing surveys while limiting the access of regular users.
             </li>
             <li>
-              Helped The Benjamin Center and affiliated companies/businesses to {' '} 
-              <span>speed up</span> and <span>enhance</span> the process of SROI
-              and EI surveys.{' '}
+              Helped The Benjamin Center and affiliated companies/businesses to{' '}
+              speed up and enhance the process of SROI and EI surveys.{' '}
             </li>
           </ul>
         </div>
