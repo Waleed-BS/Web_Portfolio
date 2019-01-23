eb06bbb2cffece56cc2c24c5de94f67122e4035b
Waleed-BS <waleed-bs@live.com>
2019-01-23 01:39:19 -0500

    style: prettier format

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -3,7 +3,7 @@ import { createStore as reduxCreateStore } from 'redux'
 const reducer = (state, action) => {
   // console.log('action', action)
   // console.log('state', state)
-  if (action.type === 'SET_HOVER') {    
+  if (action.type === 'SET_HOVER') {
     return Object.assign({}, state, {
       isHovered: action.isHovered,
     })
